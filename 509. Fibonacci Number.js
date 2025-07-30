f4c6da796052a4c0bfe276bf4d7283590e55a4ad
mdmusaalkafi <97037598+mdmusaalkafi@users.noreply.github.com>
2025-07-30 23:19:11 +0600

    Create 509. Fibonacci Number.js
    might seen three lines of code at a glance, but here the in depth thinking is  very deep, setting the base case of n <= 1, and recursive case of calling fib is the main idea here

---
+++ 509. Fibonacci Number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n <= 1)
+    {
+        return n;
+    }
+    {
+        return fib(n - 1) + fib(n - 2);
+    }
+};
+
+// n = 10;
+
+// console.log(fib(n));
